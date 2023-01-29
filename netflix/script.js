dfdf69d5876d8c475629463c954adf3d51d2ae21
Ibra <imemisagic@gmail.com>
2023-01-29 13:09:08 +0100

    new changes

--- netflix/script.js
+++ netflix/script.js
@@ -12,22 +12,3 @@ for (let i = 0; i < hiddenTextBox.length; i++) {
     iconBtn[i].classList.toggle("hide");
   });
 }
-
-// input
-const inputStarter = document.querySelector(".starter-input");
-const inputBtn = document.querySelector(".header-button");
-
-let displayMsg = function (msg) {
-  let al = document.createElement("p");
-  startSection.appendChild(al);
-  al.setAttribute("class", "temporary");
-  al.textContent = msg
-};
-
-inputBtn.addEventListener("click", function () {
-  if (!inputStarter.value) {
-    displayMsg("Please insert Your Email Address!");
-  } else if (inputStarter.value) {
-    displayMsg("Thank You!");
-  }
-});
